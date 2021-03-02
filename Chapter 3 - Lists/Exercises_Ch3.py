4fedb0030d60d3217d0634b8c6cf3ac460e7b919
Melissa Bischoping <59669210+securitysphynx@users.noreply.github.com>
2021-03-01 19:24:43 -0500

    Adding Ch. 3 and 4 directories

---
+++ Chapter 3 - Lists/Exercises_Ch3.py
@@ -0,0 +1,19 @@
+# 3-1 : Store the names of friends in a list called names. Print each person's name by accessing each element, one at a time. 
+
+# 3-2 : Start with the list from exercise 3-1, but instead of printing each person's name, print a message to them. 
+
+# 3-3 : Think of your favorite mode of transportation and make a list that stores several examples. Use your list to print a series of statements about the items. 
+
+# 3-4 :  If you could invite anyone, living or dead, to dinner, who would you invite? Make a list including at least 3 people. Print a message inviting them to dinner. 
+
+# 3-5 : Start with the program from exercise 3-4, add a print statement at the end stating the name of a guest who cannot attend. Modify the list, replacing the name of the guest who cannot attend with the name of a new person.  Print a second set of invitation messages, one for each person still in the list. 
+
+# 3-6 : Starting with the last exercises program, add a print statement informating people that you found a bigger dinner table.  Use insert() to add a guest to the begging and middle of your list. Use append() to add one to the end of the list. Print new invitation messages. 
+
+# 3-7 : Start with the work from exercise 3-6.  Add a new line that prints a message saying you can only invite two people for dinner.  Use pop() to remove guests until only two names remain. Each time, print a message informing the person they cannot attend. Then print a message informing the two remainign they are still invited.  Use del to remove the last names, so you have an empty list. Print the list to verify it's empty.
+
+# 3-8 : Think of five places in the world you'd like to visit.  Store the locations in a list. Print list in original order.  Use sorted() to print alphabetical order.  Reprint in original order.  Use sorted() to print the list in reverse alphabetical order. Print the original order again. Use reverse() to change th eorder. Print to show it's order has changed.  Use reverse() again to change it back to the original order. Use sort() to change the list permanently. Print.  Use sort() to reverse the order and store it permanently.  
+
+# 3-9 : Working with one of the exercise 3-4 through 3-7 exercises, use len() to print a message indicating the number of people being invited to dinner. 
+
+# 3-10 : Write a program that creates a list cntaining items, and then use each function introduced so far in chapter 3 at least one time. That includes: .title(), index positions (including -1), append(), insert(), del, pop(), remove(), sort(), sorted(), reverse()
